d0061cfe6810b242128e0e5396a5b8e0f506769c
giuem <453133693@qq.com>
2017-04-19 23:38:12 +0800

    TODO direction

--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,26 @@
 #include "player.h"
+
+Player::Player() {
+
+}
+
+Player::ArrowKey Player::getDirection() {
+    time_t maxTime = 0;
+    ArrowKey currentKey = ArrowKey::NONE;
+    for (int i = 0; i < 4; ++i) {
+        if (this->_keyRecord[i] > maxTime) {
+            maxTime = this->_keyRecord[i];
+            currentKey = static_cast<ArrowKey>(i);
+        }
+    }
+    return currentKey;
+}
+
+void Player::setDirection(Player::ArrowKey k) {
+    this->_keyRecord[k] = std::time(nullptr);
+}
+
+void Player::removeDirection(Player::ArrowKey k) {
+    this->_keyRecord[k] = 0;
+}
+
--- src/player.h
+++ src/player.h
@@ -3,20 +3,31 @@
 
 #include "entity.h"
 #include <string>
+#include <ctime>
 
 class Player : public Entity {
 public:
-    Player() = default;
+    Player();
 
     ~Player() = default;
+
+    enum ArrowKey {
+        LEFT = 0, RIGHT = 1, UP = 2, DOWN = 3, NONE = -1
+    };
 
     void setName(std::string name) { this->_name = name; }
 
     std::string getName() const { return this->_name; }
 
+    ArrowKey getDirection();
+
+    void setDirection(ArrowKey k);
+
+    void removeDirection(ArrowKey k);
+
 private:
     std::string _name;
+    std::time_t _keyRecord[4] = {0};
 };
-
 
 #endif //SERVER_PLAYER_H
