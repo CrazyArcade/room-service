980d5da4248babd398caefb7ed323bd7ab64ba0f
giuem <453133693@qq.com>
2017-04-22 00:24:08 +0800

    add controller

---
+++ src/controller/game.h
@@ -0,0 +1,10 @@
+#ifndef SERVER_GAME_H
+#define SERVER_GAME_H
+
+
+class game {
+
+};
+
+
+#endif //SERVER_GAME_H
---
+++ src/controller/room.h
@@ -0,0 +1,10 @@
+#ifndef SERVER_ROOM_H
+#define SERVER_ROOM_H
+
+
+class room {
+
+};
+
+
+#endif //SERVER_ROOM_H
