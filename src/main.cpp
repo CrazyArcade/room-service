247296bfd07f1aebafa56d70cbf637ea630b4343
giuem <453133693@qq.com>
2017-04-24 23:32:27 +0800

    add timer

--- src/main.cpp
+++ src/main.cpp
@@ -23,48 +23,18 @@ int main() {
 
 //    h.onError();
 
+    constexpr int perFrameTime = 1000 / 16;
+    Timer timer(h.getLoop());
+    timer.start([](Timer *handle) {
+        // TODO logic here
+    }, 0, perFrameTime);
+
     if (h.listen(4000)) {
         std::cout << "start" << std::endl;
     } else {
         std::cerr << "Can't listen at port 4000" << std::endl;
     }
-    h.run();
 
-//    crow::SimpleApp app;
-//
-//    std::mutex mtx;
-//    CROW_ROUTE(app, "/room")
-//            .websocket()
-//            .onopen([&](crow::websocket::connection &conn) {
-//                CROW_LOG_INFO << "new websocket connection";
-//                std::lock_guard<std::mutex> _(mtx);
-//                io->addUser(&conn);
-//            })
-//            .onclose([&](crow::websocket::connection &conn, const std::string &reason) {
-//                CROW_LOG_INFO << "websocket connection closed: " << reason;
-//                std::lock_guard<std::mutex> _(mtx);
-//                io->delUser(&conn);
-//            })
-//            .onmessage([&](crow::websocket::connection &conn, const std::string &data, bool is_binary) {
-//                std::lock_guard<std::mutex> _(mtx);
-//                io->handle(data, &conn);
-//            });
-//
-//    std::thread t1([&] {
-//        CROW_LOG_INFO << "gameLoop Start";
-//        while (1) {
-//            {
-//                std::lock_guard<std::mutex> _(mtx);
-//                // TODO
-//            }
-//            constexpr int64_t timeout = 1000 / 60;
-//            std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
-//        }
-//    });
-//    std::thread t2([&app] {
-//        app.port(4000).run();
-//    });
-//    t2.join();
-//    t1.join();
+    h.run();
     return 0;
 }
